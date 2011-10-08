d8c4584aef060835fffcc2f745e3565abca6418e
John Whitlock <John-Whitlock@ieee.org>
2011-10-08 11:20:51 -0500

    Use --verbosity instead of roll my own option

--- trif/trif_feeds/management/commands/fetch_feeds.py
+++ trif/trif_feeds/management/commands/fetch_feeds.py
@@ -2,13 +2,18 @@ from django.core.management.base import BaseCommand, CommandError
 from trif.trif_feeds import utils
 
 class Command(BaseCommand):
-    args = '--quiet'
     help = 'Fetches the source feeds'
     
     def handle(self, *args, **options):
-        q = '--quiet' in args
-        if not q: self.stdout.write('Reading 911 incidents\n')
+        v = int(options['verbosity'])
+
+        if v > 0:
+            self.stdout.write('Reading 911 incidents\n')
         utils.fetch_incidents()
-        if not q: self.stdout.write('Reading City of Tulsa closures\n')
+
+        if v > 0:
+            self.stdout.write('Reading City of Tulsa closures\n')
         utils.fetch_local_closures()
-        if not q: self.stdout.write('Success\n')
+
+        if v > 0: 
+            self.stdout.write('Success\n')
